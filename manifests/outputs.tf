e1ec67fcfa44170984ca752732317e2bc4fb5244
Lucas Campos Vieira <contato@lukerops.com>
2023-10-01 18:35:36 -0300

    adiciona manifesto de route

--- manifests/outputs.tf
+++ manifests/outputs.tf
@@ -1,7 +1,8 @@
 locals {
+  manifests     = fileset(path.module, "./customResourceDefinition/*.{yml,yaml}")
   gcp_manifests = var.config.gcp ? fileset(path.module, "./customResourceDefinition/gcp/*.{yml,yaml}") : []
 }
 
 output "manifests" {
-  value = concat(local.gcp_manifests)
+  value = concat(local.manifests, local.gcp_manifests)
 }
