40a7ec3e7f8d81ba5292d63efa6277992eea7c89
Lucas Campos Vieira <contato@lukerops.com>
2023-10-02 10:22:02 -0300

    ajusta output dos manifestos

--- manifests/outputs.tf
+++ manifests/outputs.tf
@@ -1,13 +1,13 @@
 locals {
-  core_manifests   = fileset(path.module, "./customResourceDefinition/*.{yml,yaml}")
-  gcp_manifests    = var.config.gcp ? fileset(path.module, "./customResourceDefinition/gcp/*.{yml,yaml}") : []
-  github_manifests = var.config.github ? fileset(path.module, "./customResourceDefinition/github/*.{yml,yaml}") : []
+  core_manifests   = fileset(path.module, "customResourceDefinition/*.{yml,yaml}")
+  gcp_manifests    = var.config.gcp ? fileset(path.module, "customResourceDefinition/gcp/*.{yml,yaml}") : []
+  github_manifests = var.config.github ? fileset(path.module, "customResourceDefinition/github/*.{yml,yaml}") : []
 
   manifests = flatten([local.core_manifests, local.gcp_manifests, local.github_manifests])
 }
 
 output "manifests" {
   value = [
-    for manifest in local.manifests : abspath(manifest)
+    for manifest in local.manifests : "${path.module}/${manifest}"
   ]
 }
